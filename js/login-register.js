8df4b3f902add880587f629ad5c3a9e4dd39577c
Felix ELoy Lamas <felamas@gmail.com>
2024-01-14 20:32:15 -0300

    subo archivo js login-register, ademas helper y foto de login

--- js/login-register.js
+++ js/login-register.js
@@ -1,13 +1,132 @@
+import {
+  LoadUserAdmin,
+  ValidateForm,
+  checkPassword,
+  validateEmail,
+  validateName,
+  validatePassword,
+} from "./helpers.js";
+
+LoadUserAdmin();
+let listUsers = JSON.parse(localStorage.getItem("Users"));
 //obtengo la accion a realizar, puede ser login o register
 const action = window.location.search.slice(8);
-let formLogin = document.getElementById("formLogin");
+let articleLogin = document.getElementById("articleLogin");
 let imgLogin = document.getElementById("imgLogin");
 let imgRegister = document.getElementById("imgRegister");
-let formRegister = document.getElementById("formRegister");
+let articleRegister = document.getElementById("articleRegister");
+// ----------------Regitro-----------------------------------
 if (action === "register") {
-  formLogin.className = `col-12 col-sm-6 my-auto p-2 d-none`;
+  articleLogin.className = `col-12 col-sm-6 my-auto p-2 d-none`;
   imgLogin.className = `col-12 col-sm-6 text-center d-none`;
-  imgRegister.className = `col-12 col-sm-6 text-center`;
-  formRegister.className = `col-12 col-sm-6 my-auto p-2 `;
-  let nameReggiter = document.getElementById("nameRegister");
+  imgRegister.className = `col-12 col-sm-6 text-center d-flex flex-column justify-content-center order-2 order-sm-1  `;
+  articleRegister.className = `col-12 col-sm-6 my-auto p-2 order-1 order-sm-2`;
+
+  let nameRegister = document.getElementById("nameRegister");
+  let emailRegister = document.getElementById("emailRegister");
+  let passwordRegister = document.getElementById("passwordRegister");
+  let passwordRegister2 = document.getElementById("passwordRegister2");
+
+  let formRegister = document.getElementById("formRegister");
+
+  formRegister.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (
+      ValidateForm(
+        nameRegister,
+        emailRegister,
+        passwordRegister,
+        passwordRegister2
+      )
+    ) {
+      if (listUsers.find((user) => user.email === emailRegister.value)) {
+        emailRegister.className = `mb-3 form-control is-invalid`;
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: "El correo ya existe, intenta de nuevo con otro correo.",
+          confirmButtonColor: "#0d6efd",
+          iconColor: "#dc3545",
+        });
+      } else {
+        const user = {
+          nombre: nameRegister.value,
+          email: emailRegister.value,
+          password: passwordRegister.value,
+          role: "cliente",
+        };
+        listUsers.push(user);
+        Swal.fire({
+          position: "top-center",
+          icon: "success",
+          title: "Se realizo el registro con exito.",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        localStorage.setItem("Users", JSON.stringify(listUsers));
+        setTimeout(() => {
+          window.location.replace("login-register.html?action=login");
+        }, 1500);
+      }
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "No se pudo realizar el registro, intenta de nuevo.",
+        confirmButtonColor: "#0d6efd",
+        iconColor: "#dc3545",
+      });
+    }
+  });
+  nameRegister.addEventListener("blur", () => validateName(nameRegister));
+  emailRegister.addEventListener("blur", () => validateEmail(emailRegister));
+  passwordRegister.addEventListener("blur", () =>
+    validatePassword(passwordRegister)
+  );
+  passwordRegister2.addEventListener("blur", () =>
+    checkPassword(passwordRegister, passwordRegister2)
+  );
 }
+//---------------FIN REGISTRO --------------------------------
+//---------------LOGIN----------------------------------------
+if (action === "login") {
+  let emailLogin = document.getElementById("emailLogin");
+  let passwordLogin = document.getElementById("passwordLogin");
+  let formLogin = document.getElementById("formLogin");
+
+  formLogin.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const userSearch = listUsers.find(
+      (user) =>
+        user.email === emailLogin.value && user.password === passwordLogin.value
+    );
+
+    if (userSearch !== undefined) {
+      const userLog = {
+        nombre: userSearch.nombre,
+        email: userSearch.email,
+        role: userSearch.role,
+      };
+      sessionStorage.setItem("userLog", JSON.stringify(userLog));
+      Swal.fire({
+        position: "top-center",
+        icon: "success",
+        title: `Bienvenido ${userLog.nombre}`,
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      setTimeout(() => {
+        window.location.replace("../index.html");
+      }, 1500);
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Correo y/o contraseña incorrecta.",
+        confirmButtonColor: "#0d6efd",
+        iconColor: "#dc3545",
+      });
+    }
+  });
+}
+//---------------FIN LOGIN---------------------------------
