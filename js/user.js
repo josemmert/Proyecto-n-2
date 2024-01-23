847410c93a8441969a46f1bca234fd80f84e7c6b
zoleicasimon <zoleicas@hotmail.com>
2024-01-22 21:40:08 -0300

    Guardar cambios en user.js antes de cambiar de rama

---
+++ js/user.js
@@ -0,0 +1,12 @@
+export const userAdmin={
+    email: 'user@gmail.com',
+    role: 'admin',
+    password: '12345'
+}
+
+//para probar si funciona el carrito
+export const userInvitado={
+    email: 'user@gmail.com',
+    role: 'invitado ',
+    password: '123456'
+}
