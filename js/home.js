45e5491c695ebed6c461d7c905079d3a23d75dc2
jemmert <jozemel5@gmail.com>
2024-02-01 02:16:22 -0300

    se hicieron cambios de colores, tipo de card y mejoras en la pagina index.html

--- js/home.js
+++ js/home.js
@@ -32,22 +32,34 @@ document.addEventListener('keyup', e => {
  function ListarProductos() {
      bodyrow.innerHTML = "";
      arrayProductos.forEach((product) => {
-         bodyrow.innerHTML += `<div class="col-sm-12 col-md-4 col-lg-3 my-3 articulos">
-         <a href="#">
-           <div class="card text-bg-dark card-text">
+         bodyrow.innerHTML += `<div class="galeria col-sm-12 col-md-4 col-lg-3 my-3 d-flex justify-content-center">
+         <a href="/pages/pag-detail.html?codigo=${product.codigo}">
+         <div class="producto card-text articulos">
+             <img src="${product.urlImg}" alt="${product.modelo}">
+             <div class="cardsm">
+              <h3 class="">${product.modelo}</h3>
+              <a href="/pages/pag-detail.html?codigo=${product.codigo}" class="btn btn-primary mt-4 d-flex justify-content-center boton-detalle">Ver más   detalles</a>
+             </div>
+         </div>
+         </a>
+     </div>`;
+     });
+ }
+
+ListarProductos();
+
+{/* <div class="col-sm-12 col-md-4 col-lg-3 my-3 articulos">
+         <a href="/pages/pag-detail.html?codigo=${product.codigo}">
+         <div class="card-custom text-bg-dark card-text">
              <img
                src="${product.urlImg}"
                class="card-img"
                alt="${product.modelo}"
              />
              <div class="card-img-overlay tnone cardsm">
-               <h5 class="card-title display-5">${product.modelo}</h5>
+               <h3 class="card-title display-5">${product.modelo}</h3>
                <a href="/pages/pag-detail.html?codigo=${product.codigo}" class="btn btn-primary mt-4">Ver más detalles</a>
              </div>
            </div>
          </a>
-       </div>`;
-     });
- }
-
-ListarProductos();
+       </div> */}
