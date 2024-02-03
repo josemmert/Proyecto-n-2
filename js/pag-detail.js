8cd133b68bc67afbbb3f479731d98e1423484642
3steban99 <barrionuevo99@outlook.com>
2024-02-02 21:47:56 -0300

    cambios de estilos

--- js/pag-detail.js
+++ js/pag-detail.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (product) {
         detailbody.innerHTML = `
             <div class="container mt-5 pt-5">
-                <h3 class="my-4">${product.marca}</h3>
+                <h2 class="my-4 title-brand fw-bold">${product.marca}</h2>
             </div>
             <hr class="container">
             <div class="container">
@@ -22,62 +22,62 @@ document.addEventListener("DOMContentLoaded", function () {
                         <img class="img-fluid" src="${product.urlImg}" alt="celular">
                     </div>
                     <div class="col-12 col-lg-6 px-4">
-                        <h4 class="text-center mt-2 mb-5">${product.modelo}</h4>
+                        <h3 class="text-center mt-2 mb-5 title-product fw-bold">${product.modelo}</h3>
                         <div class="py-4">
-                            <p>${product.descripcion}</p>
+                            <p class='fs-5 text-product'>${product.descripcion}</p>
                         </div>
                         <hr>
-                        <div class="d-flex justify-content-around py-3">
-                            <p><span class="fw-bold">Precio: </span>${product.precio}</p>
-                            <p><span class="fw-bold">Stock: </span>${product.stock}</p>
+                        <div class="d-flex justify-content-around py-3 fs-5">
+                            <p class='number-product fs-5 fw-bold'><span class="fw-bold text-product">Precio: </span>${product.precio}</p>
+                            <p class='number-product fs-5 fw-bold'><span class="fw-bold text-product">Stock: </span>${product.stock}</p>
                         </div>
                         <div class="d-flex justify-content-center mb-3 mb-lg-0">
-                            <button id="btn-add-cart" class="btn btn-custom-add fw-bold">Agregar al carrito</button>
+                            <button id="btn-add-cart" class="btn btn-custom-add fw-bold fs-4">Agregar al carrito</button>
                         </div>
                     </div>
                 </div>
             </div>
             <hr class="container">
             <div class="container pb-5">
-                <p class="h4 mb-5">Características técnicas</p>
+                <p class="fs-3 mb-5 text-product fw-bold">Características técnicas</p>
                 <div class="row">
                     <div class="col-6 col-lg-4 mx-auto d-flex flex-column justify-content-between">
                         <div class="d-flex align-items-center">
                             <div class="col-3 col-lg-2 text-center">
-                                <i class="fas fa-mobile-screen fa-2x"></i>
+                                <i class="fas fa-mobile-screen fa-2x icon-product"></i>
                             </div>
                             <div>
-                                <p class="mb-0">Tamaño de pantalla:</p>
-                                <p class="">${product.pantalla}</p>
+                                <p class="mb-0 fs-6 fw-bold">Tamaño de pantalla:</p>
+                                <p class="fs-5">${product.pantalla}</p>
                             </div>
                         </div>
                         <div class="d-flex align-items-center">
                             <div class="col-3 col-lg-2 text-center">
-                                <i class="fa-solid fa-microchip fa-2x"></i>
+                                <i class="fa-solid fa-microchip fa-2x icon-product"></i>
                             </div>
                             <div>
-                                <p class="mb-0">Memoria:</p>
-                                <p class="">${product.memoria}</p>
+                                <p class="mb-0 fs-6 fw-bold">Memoria:</p>
+                                <p class="fs-5">${product.memoria}</p>
                             </div>
                         </div>
                     </div>
                     <div class="col-6 col-lg-4 mx-auto d-flex flex-column justify-content-between">
                         <div class="d-flex align-items-center">
                             <div class="col-3 col-lg-2 text-center">
-                                <i class="fas fa-hdd fa-2x"></i>
+                                <i class="fas fa-hdd fa-2x icon-product"></i>
                             </div>
                             <div>
-                                <p class="mb-0">Almacenamiento:</p>
-                                <p class="">${product.almacenamiento}</p>
+                                <p class="mb-0 fs-6 fw-bold">Almacenamiento:</p>
+                                <p class="fs-5">${product.almacenamiento}</p>
                             </div>
                         </div>
                         <div class="d-flex align-items-center">
                             <div class="col-3 col-lg-2 text-center">
-                                <i class="fas fa-camera fa-2x"></i>
+                                <i class="fas fa-camera fa-2x icon-product"></i>
                             </div>
                             <div>
-                                <p class="mb-0">Cámara:</p>
-                                <p class="">${product.camara}</p>
+                                <p class="mb-0 fs-6 fw-bold">Cámara:</p>
+                                <p class="fs-5">${product.camara}</p>
                             </div>
                         </div>
                     </div>
