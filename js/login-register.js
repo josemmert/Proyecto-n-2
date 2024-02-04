d15613a6cbb3feb56fe4f48ea6462e9b43a9e660
Felix ELoy Lamas <felamas@gmail.com>
2024-02-04 16:35:12 -0300

    se agrego la ruta de la pagina de error a los link, y se corrigio un error en el logo del navbar

--- js/login-register.js
+++ js/login-register.js
@@ -165,7 +165,7 @@ togglePasswordL.addEventListener("click", function () {
       '<path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/><path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>';
   }
 });
-// Mostrar el icono cuando se comienza a escribir
+
 passwordInputL.addEventListener("input", function () {
   togglePasswordL.style.display =
     passwordInputL.value.length > 0 ? "block" : "none";
@@ -195,7 +195,6 @@ passwordInput.addEventListener("input", function () {
 let passwordInput2 = document.getElementById("passwordRegister2");
 let togglePassword2 = document.getElementById("togglePassword2");
 
-// Función para mostrar/ocultar la contraseña
 togglePassword2.addEventListener("click", function () {
   if (passwordInput2.type === "password") {
     passwordInput2.type = "text";
