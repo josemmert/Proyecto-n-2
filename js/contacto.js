f8295340c244a5a8ebc61beee61a1ee972c1203b
jemmert <jozemel5@gmail.com>
2024-02-05 17:13:40 -0300

    se cambio el estilo de algunos botones, correction en nombres de clases y detalles esteticos en el mensaje de alerta de la pagina de contactos, entre otras correcciones

--- js/contacto.js
+++ js/contacto.js
@@ -1,78 +1,74 @@
-let formulario = document.getElementById('contact-form');
+let formulario = document.getElementById("contact-form");
 
-formulario.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    enviarFormulario();
-    
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+  enviarFormulario();
 });
 
 function validarNombre() {
-    let nombre = document.getElementById('nombre').value;
-    let errorNombre = document.getElementById('errorNombre');
-    const regexName = /^[a-zA-ZáéíóúüñÁÉÍÓÚÜÑ\s']+$/;
+  let nombre = document.getElementById("nombre").value;
+  let errorNombre = document.getElementById("errorNombre");
+  const regexName = /^[a-zA-ZáéíóúüñÁÉÍÓÚÜÑ\s']+$/;
 
-    if (nombre.trim() !== '' && !regexName.test(nombre)) {
-        errorNombre.innerHTML = 'Por favor, ingrese su nombre.';
-        return false;
-    } else {
-        errorNombre.innerHTML = '';
-        return true;
-    }
+  if (nombre.trim() !== "" && !regexName.test(nombre)) {
+    errorNombre.innerHTML = "Por favor, ingrese su nombre.";
+    return false;
+  } else {
+    errorNombre.innerHTML = "";
+    return true;
+  }
 }
 
 function validarEmail() {
-    let email = document.getElementById('email').value;
-    let errorEmail = document.getElementById('errorEmail');
+  let email = document.getElementById("email").value;
+  let errorEmail = document.getElementById("errorEmail");
+  let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    // Patrón simple para validar el formato del correo electrónico
-    let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (!emailPattern.test(email)) {
-        errorEmail.innerHTML = 'Por favor, ingrese un correo electrónico válido.';
-        return false;
-    } else {
-        errorEmail.innerHTML = '';
-        return true;
-    }
+  if (!emailPattern.test(email)) {
+    errorEmail.innerHTML = "Por favor, ingrese un correo electrónico válido.";
+    return false;
+  } else {
+    errorEmail.innerHTML = "";
+    return true;
+  }
 }
 
 function validarConsulta() {
-    let consulta = document.getElementById('consulta').value;
-    let errorConsulta = document.getElementById('errorConsulta');
+  let consulta = document.getElementById("consulta").value;
+  let errorConsulta = document.getElementById("errorConsulta");
 
-    if (consulta.trim() === '') {
-        errorConsulta.innerHTML = 'Por favor, ingrese su consulta u opinión.';
-        return false;
-    } else {
-        errorConsulta.innerHTML = '';
-        return true;
-    }
+  if (consulta.trim() === "") {
+    errorConsulta.innerHTML = "Por favor, ingrese su consulta u opinión.";
+    return false;
+  } else {
+    errorConsulta.innerHTML = "";
+    return true;
+  }
 }
 
 function enviarFormulario() {
-    // Llamar a las funciones de validación
-    let nombreValido = validarNombre();
-    let emailValido = validarEmail();
-    let consultaValida = validarConsulta();
+  // Llamar a las funciones de validación
+  let nombreValido = validarNombre();
+  let emailValido = validarEmail();
+  let consultaValida = validarConsulta();
 
-    // Si todas las validaciones son exitosas, puedes enviar el formulario
-    if (nombreValido && emailValido && consultaValida) {
-        Swal.fire({
-            position: "top-center",
-            color: "#212121",
-            icon: "success",
-            title: `consulta enviada`,
-            showConfirmButton: false,
-            timer: 1500,
-          });
-          formulario.reset();
-        // Aquí puedes agregar código para enviar el formulario a un servidor, por ejemplo.
-    } else {
-        Swal.fire({
-            imageUrl: "../img/error.png",
-            text: "formulario erroneo",
-            confirmButtonColor: "#0d6efd",
-            color: "#212121",
-          });
-    }
+  // Si todas las validaciones son exitosas, se puede enviar el formulario
+  if (nombreValido && emailValido && consultaValida) {
+    Swal.fire({
+      position: "top-center",
+      color: "#212121",
+      icon: "success",
+      title: `consulta enviada`,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    formulario.reset();
+  } else {
+    Swal.fire({
+      imageUrl: "../img/error.png",
+      text: "formulario erroneo",
+      confirmButtonColor: "#0d6efd",
+      color: "#212121",
+    });
+  }
 }
